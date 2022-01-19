80dde444b1e20499ca36be4564af2aabb2df104d
eunsunshin <eunsonny@eunsunshinui-MacBookPro.local>
2022-01-19 14:53:58 +0900

    Add: webpack 설치 및 webpack.config.js 세팅 추가

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const fs = require('fs');
+
+config = {
+  mode: process.env.NODE_ENV,
+  output: {
+    path: path.resolve(__dirname, 'imsDesign'),
+    library: {
+      name: 'IMS-design',
+      type: 'umd',
+    }
+  },
+  devServer: {
+    hot: true,
+    host: 'localhost',
+    port: 5000
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(js|jsx|ts|tsx)$/,
+  //       exclude: /node_modules/,
+  //       use: ['babel-loader', 'ts-loader'],
+  //     },
+  //   ]
+  // }
+}
+
+const setEntry = () => {
+  const entry = {};
+  const components = fs
+    .readdirSync(`./components/${process.env.CLIENT}`, { withFileTypes: true })
+    .filter(name => {
+      return name.isDirectory() === true
+    });
+
+  components.forEach(component => {
+    entry[`${process.env.CLIENT}`] = { 
+      import : `./components/${process.env.CLIENT}/${component.name}/index.tsx`, 
+      filename: `${process.env.CLIENT}/${component.name}/index.js` 
+    }
+  })
+
+  return config.entry = entry;
+}
+
+setEntry();
+
+module.exports = config;
