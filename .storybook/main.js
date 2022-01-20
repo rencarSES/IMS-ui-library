18bc7f7486782114c5e6d251db3f7c648dc6bfd2
eunsunshin <eunsonny@eunsunshinui-MacBookPro.local>
2022-01-20 10:39:53 +0900

    Add: eslint, prettier 설치 및 환경 세팅

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,13 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
   "framework": "@storybook/react",
   "typescript": {
     check: false,
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript'
+  },
+  core: {
+    builder: "webpack5"
   }
-}
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,17 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme="default">
+      <StylesProvider injectFirst>
+        <GlobalStyle />
+        <Story />
+      </StylesProvider>
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
